649f39f340ff1efed582d0b7a7aa0edced9889b1
unawarexi <unawarexi@gmail.com>
2024-01-20 18:37:19 +0100

    Trustly-Ecommerce

--- src/components/flashDeals/FlashCard.jsx
+++ src/components/flashDeals/FlashCard.jsx
@@ -1,84 +1,110 @@
-import React, { useState } from "react"
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import React, { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SampleNextArrow = (props) => {
-  const { onClick } = props
+  const { onClick } = props;
   return (
-    <div className='control-btn' onClick={onClick}>
-      <button className='next'>
-        <i className='fa fa-long-arrow-alt-right'></i>
+    <div className="control-btn" onClick={onClick}>
+      <button className="next">
+        <i className="fa fa-long-arrow-alt-right"></i>
       </button>
     </div>
-  )
-}
+  );
+};
 const SamplePrevArrow = (props) => {
-  const { onClick } = props
+  const { onClick } = props;
   return (
-    <div className='control-btn' onClick={onClick}>
-      <button className='prev'>
-        <i className='fa fa-long-arrow-alt-left'></i>
+    <div className="control-btn" onClick={onClick}>
+      <button className="prev">
+        <i className="fa fa-long-arrow-alt-left"></i>
       </button>
     </div>
-  )
-}
+  );
+};
 const FlashCard = ({ productItems, addToCart }) => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const increment = () => {
-    setCount(count + 1)
-  }
+    setCount(count + 1);
+  };
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    autoplay: true,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
-  }
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+          autoplay: true,
+        },
+      },
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          autoplay: true,
+        },
+      },
+    ],
+  };
 
   return (
     <>
       <Slider {...settings}>
         {productItems.map((productItems) => {
           return (
-            <div className='box'>
-              <div className='product mtop'>
-                <div className='img'>
-                  <span className='discount'>{productItems.discount}% Off</span>
-                  <img src={productItems.cover} alt='' />
-                  <div className='product-like'>
+            <div className="box">
+              <div className="product-deals mtop">
+                <div className="img-deals">
+                  <span className="discount">{productItems.discount}% Off</span>
+                  <img src={productItems.cover} alt="" />
+
+                  <div className="product-like-deals">
                     <label>{count}</label> <br />
-                    <i className='fa-regular fa-heart' onClick={increment}></i>
+                    <i className="fa-regular fa-heart" onClick={increment}></i>
                   </div>
                 </div>
-                <div className='product-details'>
+
+                <div className="product-details">
                   <h3>{productItems.name}</h3>
-                  <div className='rate'>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
+                  <div className="rate">
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
                   </div>
-                  <div className='price'>
+                  <div className="price-deals">
                     <h4>${productItems.price}.00 </h4>
                     {/* step : 3  
                      if hami le button ma click garryo bahne 
                     */}
                     <button onClick={() => addToCart(productItems)}>
-                      <i className='fa fa-plus'></i>
+                      <i className="fa fa-plus"></i>
                     </button>
                   </div>
                 </div>
               </div>
             </div>
-          )
+          );
         })}
       </Slider>
     </>
-  )
-}
+  );
+};
 
-export default FlashCard
+export default FlashCard;
