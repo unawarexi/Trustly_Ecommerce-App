5eb1c1d43997e990977eb7ae2cffe6e5414e01d2
unawarexi <unawarexi@gmail.com>
2024-01-20 10:47:47 +0100

    Fixed

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -5,20 +5,25 @@ const Head = () => {
     <>
       <section className='head'>
         <div className='container d_flex'>
+
           <div className='left row'>
             <i className='fa fa-phone'></i>
             <label> +1384 3456 7374</label>
             <i className='fa fa-envelope'></i>
             <label> support@trustlyshop.com</label>
           </div>
+
           <div className='right row RText'>
             <label>Theme FAQ"s</label>
             <label>Need Help?</label>
+            
             <span>🏳️‍⚧️</span>
             <label>EN</label>
+
             <span>🏳️‍⚧️</span>
             <label>USD</label>
           </div>
+
         </div>
       </section>
     </>
